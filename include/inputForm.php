364ff6c40e411b8e9f2d2d4fd95fb57f25c6136b
KSH-KOR <tlsgndi1@gamil.com>
2022-05-21 23:25:58 +0900

    2 input php file created
    inputForm.php created
    displayInput.php created

---
+++ include/inputForm.php
@@ -0,0 +1,9 @@
+<form method=POST action='form-action.php'>
+
+Name <input type=text name=f_name><br>
+Gender <input type=radio name=gender value='Male'> Male 
+<input type=radio name=gender value='Female'> Female<br>
+Agreed <input type=checkbox name=agreed value='yes'><br><br>
+<input type=Submit value='Submit Data'> <input type=Reset value='Reset'>
+
+</form>
