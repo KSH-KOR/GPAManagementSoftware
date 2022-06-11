0e3ed0b994ab0e2aaf2041f7eb84bc73f33eff5f
rasp_shinhoo <kimcoding0407@gmail.com>
2022-06-11 09:39:03 +0100

    modifiedExe_py.php

--- src/templates/exec_py.php
+++ src/templates/exec_py.php
@@ -4,7 +4,9 @@ $type = $_POST['type'];
 $weight = $_POST['weight'];
 $unit = $_POST['unit'];
 $command = escapeshellcmd($make . ' ' . $type . ' ' . $weight . ' ' . $unit);
-echo $command;
 $output = shell_exec($command . ' 2>&1 &');
 echo $output;
+
+header('Location: '.'../../../wp/');
+die();
 ?>
